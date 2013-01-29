43eb2cc9b28f21d1e5a311a5b7aba1f20e5bfa51
mobilesoft365test <info@mobilesoft365.com>
2013-01-29 10:30:44 +0100

    Added references to TestClasses

---
+++ CalcTest/TestForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Calc;
+using NUnit.Framework;
+using NUnit.Extensions.Forms;
+
+namespace CalcTest
+{
+    [TestFixture]
+    class TestForm
+    {
+    }
+}
--- CalcTest/TestMethods.cs
+++ CalcTest/TestMethods.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Calc;
+using NUnit.Framework;
+using NUnit.Extensions.Forms;
 
 namespace CalcTest
 {
-    public class TestClass
+    [TestFixture]
+    public class TestMethods
     {
     }
 }
