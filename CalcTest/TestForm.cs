e0d99890398d297d55bb0adc72f2e03927e7f211
mobilesoft365test <info@mobilesoft365.com>
2013-01-29 11:01:01 +0100

    Test form override

--- CalcTest/TestForm.cs
+++ CalcTest/TestForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 using Calc;
 using NUnit.Framework;
 using NUnit.Extensions.Forms;
@@ -9,8 +10,14 @@ using NUnit.Extensions.Forms;
 namespace CalcTest
 {
     [TestFixture]
-    class TestForm
+    class TestForm : NUnitFormTest
     {
-        
+        Calc.Form1 _form1;
+        public override void Setup()
+        {
+            base.Setup();
+            var f = new Form1();
+            f.Show();
+        }
     }
 }
